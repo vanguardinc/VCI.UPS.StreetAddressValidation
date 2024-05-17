92090a02958f826fea383baf264dde353b2f8f21
Pat Hartl <Pat.H@vanguardinc.com>
2024-05-17 15:17:07 -0500

    Add tests for basic validation

--- VCI.UPS.StreetAddressValidation.Tests/BasicValidation.cs
+++ VCI.UPS.StreetAddressValidation.Tests/BasicValidation.cs
@@ -1,11 +1,41 @@
 namespace VCI.UPS.StreetAddressValidation.Tests
 {
-    public class BasicValidation
+    public class BasicValidation : TestBase
     {
+        public BasicValidation() : base() { }
+
         [Fact]
-        public void Test1()
+        public async Task ProperlyFormattedAddressShouldValidate()
         {
-            var validator = new UpsStreetAddressValidator()
+            var response = await Validator.ValidateAddressAsync("13100 W Lisbon Rd", "Brookfield", "WI", "53005", "US");
+
+            Assert.Single(response.AddressCandidates);
+
+            var candidate = response.AddressCandidates.First();
+
+            Assert.Equal("BROOKFIELD", candidate.City);
+            Assert.Equal("US", candidate.CountryCode);
+            Assert.Equal("53005", candidate.Postcode);
+            Assert.Equal("BROOKFIELD WI 53005-2507", candidate.Region);
+            Assert.Equal("WI", candidate.State);
+            Assert.Equal("13100 W LISBON RD", candidate.Street);
+        }
+
+        [Fact]
+        public async Task IncorrectlyFormattedAddressShouldValidate()
+        {
+            var response = await Validator.ValidateAddressAsync("13100 Lisbon Rd", "Brookfield", "WI", "", "US");
+
+            Assert.Single(response.AddressCandidates);
+
+            var candidate = response.AddressCandidates.First();
+
+            Assert.Equal("BROOKFIELD", candidate.City);
+            Assert.Equal("US", candidate.CountryCode);
+            Assert.Equal("53005", candidate.Postcode);
+            Assert.Equal("BROOKFIELD WI 53005-2507", candidate.Region);
+            Assert.Equal("WI", candidate.State);
+            Assert.Equal("13100 W LISBON RD", candidate.Street);
         }
     }
 }
---
+++ VCI.UPS.StreetAddressValidation.Tests/_TestBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCI.UPS.StreetAddressValidation.Tests
+{
+    public abstract class TestBase
+    {
+        protected StreetAddressValidator Validator;
+
+        protected TestBase()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            Validator = new StreetAddressValidator(configuration.GetConnectionString("Default"));
+        }
+    }
+}
